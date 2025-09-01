b5e4b4226d643c4584180925159c91547e0bf166
tteslee <eunsoo@darkmatterlabs.org>
2025-09-01 15:46:37 +0900

    Fix vite config for relative paths

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/human-machine-interaction-cube/' : './',
+  base: './',
   server: {
     port: 3000,
     open: true
